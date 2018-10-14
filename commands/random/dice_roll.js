7867845efde66a46d34591c66ed918d3cb38c443
eddy0117 <44132914+eddy0117@users.noreply.github.com>
2018-10-14 22:11:05 +0900

    Update dice_roll.js

--- commands/random/dice_roll.js
+++ commands/random/dice_roll.js
@@ -17,18 +17,28 @@ class DiceRollCommand extends commando.Command {
         //message.reply('你骰到了' + roll);
         
         
-        if(roll == 1)
-        message.channel.sendMessage({files:["https://c.kekeke.cc/t/K9jfGPC78G.gif"]});
-        if(roll == 2)
-        message.channel.sendMessage({files:["https://img.kekeke.cc/t/K87BsXAyZk.jpeg"]});
-        if(roll == 3)
-        message.channel.sendMessage({files:["https://img.kekeke.cc/t/K9jftucpYB.jpeg"]});
-        if(roll == 4)
-        message.channel.sendMessage({files:["https://v.kekeke.cc/t/K9nwFATvfJ.mp4"]});
-        if(roll == 5)
-        message.channel.sendMessage({files:["https://pbs.twimg.com/media/DpYjVdeU0AAgGFR.jpg"]});
-        if(roll == 6)
-        message.channel.sendMessage({files:["https://img.kekeke.cc/t/K9jcHQ7TN2.jpeg"]});
+        switch (roll) {
+            case 1:
+                message.channel.sendMessage({files:["https://c.kekeke.cc/t/K9jfGPC78G.gif"]});
+                break;
+            case 2:
+                message.channel.sendMessage({files:["https://img.kekeke.cc/t/K87BsXAyZk.jpeg"]});
+                break;
+            case 3:
+                message.channel.sendMessage({files:["https://img.kekeke.cc/t/K9jftucpYB.jpeg"]});
+                break;    
+            case 4:
+                message.channel.sendMessage({files:["https://v.kekeke.cc/t/K9nwFATvfJ.mp4"]});
+                break;
+            case 5:
+                message.channel.sendMessage({files:["https://pbs.twimg.com/media/DpYjVdeU0AAgGFR.jpg"]});
+                break;
+            case 6:
+                message.channel.sendMessage({files:["https://img.kekeke.cc/t/K9jcHQ7TN2.jpeg"]});
+                break;            
+            default:
+                break;
+        }
         
     }
 }
