9e900ef2bc36e1586ba7c65c14f45563aa225b89
eddy0117 <44132914+eddy0117@users.noreply.github.com>
2018-10-20 02:46:47 +0900

    Update repr.js

--- commands/random/repr.js
+++ commands/random/repr.js
@@ -1,6 +1,6 @@
 const commando = require('discord.js-commando');
+const Discord = require('discord.js');
 const reputation = require("../../reputation.json");
-const discord = require('discord.js');
 const fs = require("fs");
 class RepRollCommand extends commando.Command
 {
@@ -15,7 +15,7 @@ class RepRollCommand extends commando.Command
         );
     }
 
-    async run(message, args,addsub)
+    async run(message, args)
     {  
         //let targetUser = message.guild.members.get(args[0]);
         /*
@@ -25,26 +25,49 @@ class RepRollCommand extends commando.Command
             return;
         } 
         */ 
-        if(!reputation[message.author.id]){
-            reputation[message.author.id] = {
+        
+        if(!reputation[message.author.username]){
+            reputation[message.author.username] = {
                 rep: 100,
             };
         }
         
-        //var PutValue = message.author.send;
-        
-        var scale = Math.floor(Math.random() * 2);
-        if(scale == 1)
+        //var value = args;
+        if( args <= 0 || args == isNaN)
         {
-            reputation[message.author.id].rep = Math.floor(Math.random() * args * 5) + 1;
-            addsub = "你的名聲增加到了"
+            message.reply("輸入正確數值 ,please");
         }
-        else 
+        if( args <= reputation[message.author.username].rep && args > 0 )
         {
-            reputation[message.author.id].rep = Math.floor(Math.random() * args * 5 * -1) - 1;
-            addsub = "你的名聲減少到了"
+            
+        
+        
+            var scale = Math.floor(Math.random() * 2);
+            if(scale == 1)
+            {
+                reputation[message.author.username].rep = reputation[message.author.username].rep + Math.floor(Math.random() * 5 * args);
+                message.reply("你的名聲增加到了" + reputation[message.author.username].rep);
+                
+            }
+            else 
+            {
+                reputation[message.author.username].rep = reputation[message.author.username].rep + Math.floor(Math.random() * 5 * args * -1);
+                message.reply("你的名聲減少到了" + reputation[message.author.username].rep);
+                
+            }
+        }
+        else if( args >= reputation[message.author.username].rep )
+        {
+            message.reply('你的名聲不夠');
         } 
-        message.reply(addsub + reputation[message.author.id].rep);
+
+        if( reputation[message.author.username].rep <= 0)
+        {
+            message.reply('你名聲全沒了ㄌㄐ,恢復回名聲 50');
+            reputation[message.author.username].rep = 50;
+        }
+        
+        //message.reply(addsub + reputation[message.author.username].rep);
         fs.writeFile("reputation.json", JSON.stringify(reputation), (err) =>
         {
             if(err)
@@ -57,6 +80,3 @@ class RepRollCommand extends commando.Command
 }
 
 module.exports = RepRollCommand;
-
-
-
