54c0eeb93e3fdbc0da57d6955de51436cde10f7b
eddy0117 <44132914+eddy0117@users.noreply.github.com>
2018-11-04 21:51:31 +0900

    Update car.js

--- commands/random/car.js
+++ commands/random/car.js
@@ -12,26 +12,14 @@ class CarCommand extends commando.Command {
         );
     }
 
-    async run(message, args){
-        var roll = Math.floor(Math.random() * 50000) + 10000;
+    async run(message, args, i){
         
+    for(i = 0 ; i < = args ; i + + )
+            {            
         
-        /*
-        if(roll == 1)
-        Car = "https://www.wnacg.org/photos-slide-aid-39667.html"
-        if(roll == 2)
-        Car = "https://www.wnacg.com/photos-slide-aid-23427.html"
-        if(roll == 3)
-        Car = ""
-        if(roll == 4)
-        Car = ""
-        if(roll == 5)
-        Car = ""
-        if(roll == 6)
-        Car = ""
-        */
-        for(var i = 0 ; i < = args ; i + + )
-            {    
+            var roll = Math.floor(Math.random() * 50000) + 10000;
+        
+         
             message.reply('車車:https://www.wnacg.com/photos-slide-aid-' + roll + ".html");
             }    
     }
